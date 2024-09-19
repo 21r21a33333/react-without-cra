f274af7dc27de10dfa64d79b6248cbe67891fec7
21r21a33333 <21r21a3333@mlrinstitutions.ac.in>
2024-09-19 14:51:59 +0530

    react setup without cra

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+    render() {
+        return (
+            <div >
+                <h1>create-react-app-setup-using</h1>
+                <h2>babel+webpack+react</h2>
+            </div>
+        )
+    }
+}
+
+export default App;
